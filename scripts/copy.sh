f45cb77eb83bdc66d2a90f813114466b2960515c
Victor Hung <victorhung92@gmail.com>
2022-02-08 15:46:23 -0500

    Audio files and copy script

---
+++ scripts/copy.sh
@@ -0,0 +1,17 @@
+
+while [ true ] ; do
+read -n 1 -p "Press any key to copy files to /Volumes/Untitled/1: "
+echo "\n"
+mkdir /Volumes/Untitled/1
+for i in {1..9}
+do
+   cp ../mp3/000$i.mp3 /Volumes/Untitled/1 &&
+   echo "Copied 000$i.mp3" 
+done
+for i in {10..37}
+do
+   cp ../mp3/00$i.mp3 /Volumes/Untitled/1 &&
+   echo "Copied 00$i.mp3"
+done
+echo "done!\n"
+done
