312ab2af35e25fc84e1bd6144ce649d1d246618a
Victor Hung <victorhung92@gmail.com>
2023-02-17 10:53:29 -0500

    feat: add 2023 code, rough undocumented

--- 2023/arduino/ToobersArduino/ToobersArduino.ino
+++ 2023/arduino/ToobersArduino/ToobersArduino.ino
@@ -20,7 +20,7 @@
 #include "DFRobotDFPlayerMini.h"
 #include <FastLED.h>
 
-SoftwareSerial mySoftwareSerial(10, 11);                  // RX, TX
+SoftwareSerial mySoftwareSerial(3, 2);                    // Recieve Pin (RX), Transmit Pin (TX)
 DFRobotDFPlayerMini myDFPlayer;                           // Create myDFPlayer of the DFPlayer Class
 
 int MAX_SCORE = 100;
@@ -34,6 +34,18 @@ int score = 0;                                            // Keeps track of the 
 int sequence[101];                                        // Keeps track of the sequence
 int buttonPressed[4] = {false, false, false, false};      // State of each button
 
+int colorMap[4][3] = {
+  {255, 0, 0},
+  {0, 255, 0},
+  {0, 0, 255},
+  {255, 255, 0}
+};
+
+#define LED_PIN 4
+#define NUM_LEDS 4
+
+CRGB leds[NUM_LEDS];
+
 int playerSequenceIndex = 0;                              // When the user is playing back the sequence, this keeps track of where in the sequence they are
 
 /*
@@ -43,14 +55,18 @@ int playerSequenceIndex = 0;                              // When the user is pl
  * This function will only run once, after each powerup or reset of the Arduino board.
  */
 void setup() {
+
+	FastLED.addLeds<WS2812,LED_PIN,RGB>(leds,NUM_LEDS);
+	FastLED.setBrightness(255);
+  
   mySoftwareSerial.begin(9600);                           // Initiate communication with the DFPlayer
   Serial.begin(115200);                                   // Mostly used for debugging
 
-  for (int i = 2; i < 6; i++) {                           // Set inputs 2, 3, 4, 5 to be OUTPUT pins (LEDs)           
-    pinMode(i, OUTPUT);
-  }
+  // for (int i = 2; i < 6; i++) {                           // Set inputs 2, 3, 4, 5 to be OUTPUT pins (LEDs)           
+  //   pinMode(i, OUTPUT);
+  // }
   
-  for (int i = 6; i < 10; i++) {                          // Set inputs 6, 7, 8, 9 to be INPUT pins (Buttons)
+  for (int i = 5; i < 9; i++) {                          // Set inputs 5, 6, 7, 8 to be INPUT pins (Buttons)
     pinMode(i, INPUT_PULLUP);
   }
 
@@ -70,20 +86,39 @@ void setup() {
 
   delay(1000);
   
-  myDFPlayer.volume(20);                                   // Set volume value. From 0 to 30
+  myDFPlayer.volume(25);                                   // Set volume value. From 0 to 30
  
   playAudio(1);                                            // Play the first mp3! Exciting stuff
 
   delay(240);                                              // This delay and digital write is timed to turn each light on and off
-  for (int i = 2; i < 6; i ++) {                           // to match up with the sound effects
+  for (int i = 0; i < 4; i ++) {                           // to match up with the sound effects
     for (int j = 0; j < (i == 5 ? 14 : 5); j ++) {         // There's an operation in there called the ternary operator.
-      digitalWrite(i, HIGH);                               // If we're on the last LED (i.e. led 5) loop more times so we can time it with the music
+      setLedSimple(i);                                     // If we're on the last LED (i.e. led 5) loop more times so we can time it with the music
       delay(50);
-      digitalWrite(i, LOW);
+      clearLed(i);
       delay(50);
     }
   }
-  delay(5000);
+
+  for (int hue = 0; hue < 400; hue ++) {
+    for(int i = 0; i < 4; i++) {
+      // Set the i'th led to red 
+      leds[i] = CHSV((hue * 4 + i * 20) % 255, 255, 255);
+      // Show the leds
+      FastLED.show(); 
+      // now that we've shown the leds, reset the i'th led to black
+      // leds[i] = CRGB::Black;
+      // fadeall();
+      // Wait a little bit before we loop around and do it again
+    }
+    delay(10);
+  }
+  clearLed(0);
+  clearLed(1);
+  clearLed(2);
+  clearLed(3);
+
+  // delay(5000);
   state = 1;                                               // We're all set with this state. Let's set the state to 1 to start the sequence playing
 }
 
@@ -96,13 +131,19 @@ void setup() {
  */
 void loop()
 {
+  Serial.println(readButton(0));
+  Serial.println(readButton(1));
+  Serial.println(readButton(2));
+  Serial.println(readButton(3));
+  Serial.println("--");
+
   if (state == 1) {                                        // This is where we'll play the sequence to the user
     Serial.println(score);
     for (int i = 0; i < score + 1; i++) {                  // The number of tones played is one more than the score
-      playAudio(sequence[i]);                              // Play the tone as defined in our initial 100 tones.
-      digitalWrite(sequence[i], HIGH);
+      playAudio(sequence[i]+2);                              // Play the tone as defined in our initial 100 tones.
+      setLedSimple(sequence[i]);
       delay(400);
-      digitalWrite(sequence[i], LOW);
+      clearLed(sequence[i]);
       delay(100);
     }
     playerSequenceIndex = 0;                               // Reset the player sequence number
@@ -110,17 +151,17 @@ void loop()
   }
 
   if (state == 2) {                                        // If we're in this state, we're listening to the user input          
-    for (int i = 2; i < 6; i ++) {
-      if (digitalRead(i+4) == LOW && buttonPressed[i-2] == false) {
-        buttonPressed[i-2] = true;                         // This line is used for debouncing. We can use this technique to prevent
+    for (int i = 0; i < 4; i ++) {
+      if (readButton(i) == LOW && buttonPressed[i] == false) {
+        buttonPressed[i] = true;                          // This line is used for debouncing. We can use this technique to prevent
                                                            // a button from being pressed 'multiple times', since the input may be noisy.
                                                            // Once the user presses it, this code will no longer run until they've fully 
                                                            // 'released' the button (see the else if statement below)
                                                            
-        playAudio(i);                                      
-        digitalWrite(i, HIGH);
-        delay(450);
-        digitalWrite(i, LOW);
+        playAudio(i+2);        
+        setLedSimple(i);
+        delay(350);
+        clearLed(i);
       
         if (sequence[playerSequenceIndex] == i) {
           playerSequenceIndex += 1;
@@ -142,10 +183,10 @@ void loop()
           state = 3;                                       // Player has lost! Update the state to now be in the 'waiting for reset'
           playAudio(7); 
           delay(200);
-          for (int k = 2; k < 6; k ++) {
-            digitalWrite(7-k, HIGH);
+          for (int k = 0; k < 4; k ++) {
+            setLedSimple(3-k);
             delay(200);
-            digitalWrite(7-k, LOW);
+            clearLed(3-k);
           }
           delay(3000);
           
@@ -155,25 +196,27 @@ void loop()
           delay(2500);
         }
         
-      } else if (digitalRead(i+4) == HIGH && buttonPressed[i-2] == true) {
-        buttonPressed[i-2] = false;                        // Part of the debouncing system as mentioned above
+      } else if (readButton(i) == HIGH && buttonPressed[i] == true) {
+        buttonPressed[i] = false;                        // Part of the debouncing system as mentioned above
       }
     }
   }
 
   if (state == 3) {                                        // Waiting for player to press 
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    if (digitalRead(6) == LOW || digitalRead(7) == LOW || digitalRead(8) == LOW || digitalRead(9) == LOW) {
+
+    setLedSimple(0);
+    setLedSimple(1);
+    setLedSimple(2);
+    setLedSimple(3);
+
+    if (readButton(0) == LOW || readButton(1) == LOW || readButton(2) == LOW || readButton(3) == LOW) {
       playerSequenceIndex = 0;
       score = 0;
       state = 1;
-      digitalWrite(2, LOW);
-      digitalWrite(3, LOW);
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
+      clearLed(0);
+      clearLed(1);
+      clearLed(2);
+      clearLed(3);
       delay(1000);
     }
   }
@@ -189,7 +232,7 @@ void generateSequence() {
                                                           // We can use this noise to generate a random number.
     
   for (int i = 0; i < MAX_SCORE + 1; i++) {               // Pre-set 100 random tones to be our sequence
-    sequence[i] = random(2, 6);
+    sequence[i] = random(0, 4);
   }
 }
 
@@ -226,7 +269,22 @@ void playScoreSound(int s) {
  * helper function that sets an LED to a specific color using the WS2812B Library
  */
 void setLed(int ledId, int r, int g, int b) {
-  
+	leds[ledId] = CRGB(r, g, b);
+  FastLED.show();
+}
+
+void setLedSimple(int ledId) {
+	leds[ledId] = CRGB(colorMap[ledId][0], colorMap[ledId][1], colorMap[ledId][2]);
+  FastLED.show();
+}
+
+void clearLed(int ledId) {
+	leds[ledId] = CRGB(0, 0, 0);
+  FastLED.show();
+}
+
+boolean readButton(int buttonId) {
+  return digitalRead((5 - buttonId) + 3);
 }
 
 void playAudio(int s) {
